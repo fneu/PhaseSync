a87ad6eaf6b6fe45ec2d55cf83606c413100500a
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-02 00:01:09 +0200

    feat: user login

--- src/PhaseSync/Program.cs
+++ src/PhaseSync/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using PhaseSync.Areas.Identity;
 using PhaseSync.Data;
 
 namespace PhaseSync
@@ -11,8 +16,15 @@ namespace PhaseSync
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
+            builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
@@ -25,12 +37,25 @@ namespace PhaseSync
                 app.UseHsts();
             }
 
+            // Migrate latest database changes during startup
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider
+                    .GetRequiredService<ApplicationDbContext>();
+                
+                // Here is the migration executed
+                dbContext.Database.Migrate();
+            }
+
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
+            app.MapControllers();
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
 
