f15b72422ef2f1fa1d9427aeef58e8a35828f618
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-07 14:38:42 +0200

    feat: settings page showing userID

---
+++ src/PhaseSync/Pages/Settings.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace PhaseSync.Pages
+{
+
+    public partial class Settings : ComponentBase
+    {
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
+        public string UserID { get; set; } = "initial";
+
+        protected async override Task OnInitializedAsync()
+        {
+            var authstate = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            if (!authstate.User.Identity!.IsAuthenticated)
+            {
+                UserID = "not authenticated";
+                return;
+            }
+            UserID = authstate.User.Claims.First().Value;
+        }
+    }
+}
