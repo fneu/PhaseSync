ffbf7d5f24336f281fbe611a67e9dd054f5c8f19
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-13 17:03:59 +0200

    feat: add time zone conversion

---
+++ src/PhaseSync.Core/Units/LocalTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Yaapii.Atoms.Scalar;
+
+namespace PhaseSync.Core.Units
+{
+    /// <summary>
+    /// Converts a UTC time like '2023-06-25T07:00:00Z'
+    /// to user local time according to the user's time zone.
+    /// </summary>    
+    public sealed class LocalTime : ScalarEnvelope<string>
+    {
+        public LocalTime(string utcTime, string timeZone) : base(
+            () => 
+            TimeZoneInfo.ConvertTimeFromUtc(
+                DateTime.ParseExact(utcTime,
+                    "yyyy-MM-ddTHH:mm:ssZ",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal
+                ),
+            TimeZoneInfo.FindSystemTimeZoneById(timeZone)
+            ).ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture)
+        )
+        { }
+    }
+}
---
+++ tests/Test.PhaseSync.Core/Units/LocalTimeTests.cs
@@ -0,0 +1,13 @@
+﻿using PhaseSync.Core.Units;
+
+namespace Test.PhaseSync.Core.Units
+{
+    public sealed class LocalTimeTests
+    {
+        [Fact]
+        public void ConvertsToBerlinTime()
+        {
+            Assert.Equal("2023-06-25T09:00", new LocalTime("2023-06-25T07:00:00Z", "Europe/Berlin").Value());
+        }
+    }
+}
