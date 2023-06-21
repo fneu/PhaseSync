3d1c6099855463495ba3f6e1707dd63d36e4cdbc
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-21 13:48:11 +0200

    feat: target time input

---
+++ src/PhaseSync.Core/Entity/PhasedTarget/Input/Time.cs
@@ -0,0 +1,33 @@
+﻿using PhaseSync.Core.Units;
+using Xive;
+using Yaapii.Atoms.Scalar;
+
+namespace PhaseSync.Core.Entity.PhasedTarget.Input
+{
+    public sealed class Time : EntityInputEnvelope<IHoneyComb>
+    {
+        private const string KEY = "timestamp";
+
+        public Time(string utcTime, string localTimeZone) : base(
+            (comb) => comb.Props().Refined(KEY, new LocalTime(utcTime, localTimeZone).Value())
+
+            )
+        { }
+
+        public sealed class Has : ScalarEnvelope<bool>
+        {
+            public Has(IEntity<IHoneyComb> target) : base(
+               () => target.Memory().Props().Names().Contains(KEY)
+            )
+            { }
+        }
+
+        public sealed class Of : ScalarEnvelope<string>
+        {
+            public Of(IEntity<IHoneyComb> target) : base(
+                () => target.Memory().Props().Value(KEY)
+            )
+            { }
+        }
+    }
+}
---
+++ tests/Test.PhaseSync.Core/Entity/PhasedTarget/TimeTests.cs
@@ -0,0 +1,18 @@
+﻿using PhaseSync.Core.Entity.PhasedTarget;
+using PhaseSync.Core.Entity.PhasedTarget.Input;
+using Xive.Hive;
+
+namespace Test.PhaseSync.Core.Entity.PhasedTarget
+{
+    public sealed class TimeTests
+    {
+
+        [Fact]
+        public void SetsLocalTime()
+        {
+            var target = new PhasedTargetOf(new RamHive("test_user_id"), "test_target_id");
+            target.Update(new Time("2023-06-25T07:00:00Z", "Europe/Berlin"));
+            Assert.Equal("2023-06-25T09:00", new Time.Of(target).Value());
+        }
+    }
+}
