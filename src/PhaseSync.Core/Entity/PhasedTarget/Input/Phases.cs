5f5c7ca0b0c5d60a715ab14a581780c579690c9a
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-21 16:45:53 +0200

    feat: target phases input

---
+++ src/PhaseSync.Core/Entity/PhasedTarget/Input/Phases.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json.Nodes;
+using Xive;
+using Yaapii.Atoms.IO;
+using Yaapii.Atoms.Scalar;
+using Yaapii.Atoms.Text;
+
+namespace PhaseSync.Core.Entity.PhasedTarget.Input
+{
+
+    public sealed class Phases : EntityInputEnvelope<IHoneyComb>
+    {
+        private const string KEY = "phases";
+
+        public Phases(IEnumerable<JsonNode> phases) : base(
+            comb => comb.Cell(KEY).Update(
+                new InputOf(
+                    new JsonArray(
+                        phases.ToArray()
+                    ).ToJsonString()
+                )
+            )
+
+        )
+        { }
+
+        public sealed class Has : ScalarEnvelope<bool>
+        {
+            public Has(IEntity<IHoneyComb> target) : base(
+                () => target.Memory().Cell(KEY).Content().Length != 0
+            )
+            { }
+        }
+
+        public sealed class Of : ScalarEnvelope<JsonNode>
+        {
+            public Of(IEntity<IHoneyComb> target) : base(
+                () => JsonNode.Parse(
+                    new TextOf(
+                        target.Memory().Cell(KEY).Content()
+                    ).AsString()
+                )!
+            )
+            { }
+        }
+    }
+}
---
+++ tests/Test.PhaseSync.Core/Entity/PhasedTarget/PhasesTests.cs
@@ -0,0 +1,27 @@
+﻿using PhaseSync.Core.Entity.PhasedTarget;
+using PhaseSync.Core.Entity.PhasedTarget.Input;
+using System.Text.Json.Nodes;
+using Xive.Hive;
+using Yaapii.Atoms.Enumerable;
+
+namespace Test.PhaseSync.Core.Entity.PhasedTarget
+{
+    public sealed class PhasesTests
+    {
+        [Fact]
+        public void SetsPhases()
+        {
+            var target = new PhasedTargetOf(new RamHive("test_user_id"), "test_target_id");
+            Assert.False(new Phases.Has(target).Value());
+
+            target.Update(
+                new Phases(
+                    new ManyOf<JsonNode>(
+                        JsonObject.Parse("{\"name\": \"value\"}")!
+                    )
+                )
+            );
+            Assert.Equal("[\r\n  {\r\n    \"name\": \"value\"\r\n  }\r\n]", new Phases.Of(target).Value().ToString());
+        }
+    }
+}
