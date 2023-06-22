87affda1f379574aed43bb2faec86a2eae0baeed
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-22 12:28:19 +0200

    feat: polar duration format

---
+++ src/PhaseSync.Core/Units/PolarDuration.cs
@@ -0,0 +1,17 @@
+﻿using Yaapii.Atoms.Text;
+
+namespace PhaseSync.Core.Units
+{
+    public sealed class PolarDuration : TextEnvelope
+    {
+        public PolarDuration(int seconds) : base(() =>
+            {
+                var t = TimeSpan.FromSeconds(seconds);
+                return $"{t.Hours:D2}:{t.Minutes:D2}:{t.Seconds:D2}";
+            },
+            false)
+        {
+
+        }
+    }
+}
---
+++ tests/Test.PhaseSync.Core/Units/PolarDurationTests.cs
@@ -0,0 +1,19 @@
+﻿using PhaseSync.Core.Units;
+
+namespace Test.PhaseSync.Core.Units
+{
+    public sealed class PolarDurationTests
+    {
+        [Fact]
+        public void ConvertsShortDuration()
+        {
+            Assert.Equal("00:43:01", new PolarDuration(2581).AsString());
+        }
+
+        [Fact]
+        public void ConvertsLongDuration()
+        {
+            Assert.Equal("01:23:20", new PolarDuration(5000).AsString());
+        }
+    }
+}
