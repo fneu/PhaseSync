dc948605052a26604710bdb5f9a2555df86cf536
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-06-22 12:00:48 +0200

    feat: add human readable pace

---
+++ src/PhaseSync.Core/Units/Pace.cs
@@ -0,0 +1,37 @@
+﻿using PhaseSync.Core.Entity;
+using PhaseSync.Core.Entity.Settings;
+using PhaseSync.Core.Entity.Settings.Input;
+using Xive;
+using Yaapii.Atoms.Text;
+
+namespace PhaseSync.Core.Units
+{
+    public sealed class Pace : TextEnvelope
+    {
+        public Pace(double velocityMPS, IHive userHive) : this(
+            velocityMPS, new SettingsOf(userHive)
+            )
+        { }
+
+        public Pace(double velocityMPS, IEntity<IProps> settings) : this(
+            velocityMPS,
+            new ZoneUnit.Has(settings).Value() && new ZoneUnit.Of(settings).Value() == "METRIC"
+        )
+        { }
+
+        public Pace(double velocityMPS, bool metric) : base(
+            () =>
+            {
+                if (metric)
+                {
+                    return $"{(int)(1000.0 / velocityMPS / 60)}:{(int)(1000.0 / velocityMPS % 60 + 0.5):D2}";
+                }
+                else
+                {
+                    return $"{(int)(1609.34 / velocityMPS / 60)}:{(int)(1609.34 / velocityMPS % 60 + 0.5):D2}";
+                }
+            },
+            false)
+        { }
+    }
+}
---
+++ tests/Test.PhaseSync.Core/Units/PaceTests.cs
@@ -0,0 +1,29 @@
+﻿using PhaseSync.Core.Entity.Settings;
+using PhaseSync.Core.Entity.Settings.Input;
+using PhaseSync.Core.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xive.Hive;
+
+namespace Test.PhaseSync.Core.Units
+{
+    public sealed class PaceTests
+    {
+        [Fact]
+        public void ConvertsToKM()
+        {
+            var settings = new SettingsOf(new RamHive("test_user_id"));
+            settings.Update(new ZoneUnit("METRIC"));
+            Assert.Equal("4:36", new Pace(3.625, settings).AsString());
+        }
+
+        [Fact]
+        public void ConvertsToMiles()
+        {
+            Assert.Equal("7:24", new Pace(3.625, false).AsString());
+        }
+    }
+}
