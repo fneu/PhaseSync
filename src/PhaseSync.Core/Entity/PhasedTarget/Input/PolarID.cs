df0d533a5051b674f3fcb2feaf8ce009b49304a7
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-07-15 20:53:41 +0200

    feat: PolarID input

---
+++ src/PhaseSync.Core/Entity/PhasedTarget/Input/PolarID.cs
@@ -0,0 +1,34 @@
+﻿using Xive;
+using Yaapii.Atoms.Number;
+using Yaapii.Atoms.Scalar;
+using Yaapii.Atoms.Text;
+
+namespace PhaseSync.Core.Entity.PhasedTarget.Input
+{
+    public sealed class PolarID : EntityInputEnvelope<IHoneyComb>
+    {
+        private const string KEY = "polarID";
+
+        public PolarID(int id) : base(
+            (comb) => comb.Props().Refined(KEY, new TextOf(id).AsString())
+
+            )
+        { }
+
+        public sealed class Has : ScalarEnvelope<bool>
+        {
+            public Has(IEntity<IHoneyComb> target) : base(
+               () => target.Memory().Props().Names().Contains(KEY)
+            )
+            { }
+        }
+
+        public sealed class Of : ScalarEnvelope<int>
+        {
+            public Of(IEntity<IHoneyComb> target) : base(
+                () => new NumberOf(target.Memory().Props().Value(KEY)).AsInt()
+            )
+            { }
+        }
+    }
+}
---
+++ tests/Test.PhaseSync.Core/Entity/PhasedTarget/Input/PolarIDTests.cs
@@ -0,0 +1,18 @@
+﻿using PhaseSync.Core.Entity.PhasedTarget;
+using PhaseSync.Core.Entity.PhasedTarget.Input;
+using Xive.Hive;
+
+namespace Test.PhaseSync.Core.Entity.PhasedTarget.Input
+{
+    public sealed class PolarIDTests
+    {
+
+        [Fact]
+        public void SetsPolarID()
+        {
+            var target = new PhasedTargetOf(new RamHive("test_user_id"), "test_target_id");
+            target.Update(new PolarID(123));
+            Assert.Equal(123, new PolarID.Of(target).Value());
+        }
+    }
+}
