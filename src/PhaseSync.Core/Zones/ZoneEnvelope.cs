518e9eefbfd98063e4ced2e6b88bb8866267862f
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-07-29 22:16:42 +0200

    fix: use scalar in ZoneEnvelope for caching

--- src/PhaseSync.Core/Zones/ZoneEnvelope.cs
+++ src/PhaseSync.Core/Zones/ZoneEnvelope.cs
@@ -1,25 +1,31 @@
-﻿namespace PhaseSync.Core.Zones
+﻿using Yaapii.Atoms;
+using Yaapii.Atoms.Scalar;
+
+namespace PhaseSync.Core.Zones
 {
     public abstract class ZoneEnvelope : IZone
     {
-        private readonly Func<IZone> zone;
+        private readonly IScalar<IZone> zone;
 
         public ZoneEnvelope(IZone zone) : this(() => zone)
         { }
 
-        public ZoneEnvelope(Func<IZone> zone)
+        public ZoneEnvelope(Func<IZone> zone) :this(new ScalarOf<IZone>(zone))
+        { }
+
+        public ZoneEnvelope(IScalar<IZone> zone)
         {
             this.zone = zone;
         }
 
         public double Min()
         {
-            return this.zone.Invoke().Min();
+            return this.zone.Value().Min();
         }
 
         public double Max()
         {
-            return this.zone.Invoke().Max();
+            return this.zone.Value().Max();
         }
     }
 }
