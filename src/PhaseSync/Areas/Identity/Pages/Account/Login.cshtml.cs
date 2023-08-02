2fc092fa66318ce61f6776a385f9bd46db699b04
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-08-02 18:42:11 +0200

    feat: login redirects to workouts page

--- src/PhaseSync/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/PhaseSync/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -103,7 +103,7 @@ namespace PhaseSync.Blazor.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/workouts");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
