0f7ba6bbdb54be6c1477de5f7123de75f6d3e711
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-08-06 20:56:40 +0200

    feat: navigate logged in users from / to /workouts

---
+++ src/PhaseSync/Pages/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace PhaseSync.Blazor.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject]
+        public AuthenticationStateProvider authenticationStateProvider { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                var authstate = await this.authenticationStateProvider.GetAuthenticationStateAsync();
+                var authenticated = authstate.User.Identity?.IsAuthenticated ?? false;
+                if (authenticated)
+                {
+                    NavigationManager.NavigateTo("/workouts");
+
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
